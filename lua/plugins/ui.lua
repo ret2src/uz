a4578ba737d7feae8b0207e3931b71d32bd3fc84
ret2src <2010034+ret2src@users.noreply.github.com>
2024-04-18 15:07:55 +0200

    push

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -57,6 +57,7 @@ return {
 	-- fancy tabs
 	{
 		"akinsho/bufferline.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
 		event = "VeryLazy",
 		keys = {
 			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
@@ -93,16 +94,5 @@ return {
 				},
 			},
 		},
-		config = function(_, opts)
-			require("bufferline").setup(opts)
-			-- Fix bufferline when restoring a session
-			-- vim.api.nvim_create_autocmd("BufAdd", {
-			-- 	callback = function()
-			-- 		vim.schedule(function()
-			-- 			pcall(nvim_bufferline)
-			-- 		end)
-			-- 	end,
-			-- })
-		end,
 	},
 }
