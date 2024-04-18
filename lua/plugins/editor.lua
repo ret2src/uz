7f16e4c00e0798b1d47bd880ad5ac2699e724137
ret2src <2010034+ret2src@users.noreply.github.com>
2024-04-18 20:56:54 +0200

    push

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -44,5 +44,49 @@ return {
 		deactivate = function()
 			vim.cmd([[Neotree close]])
 		end,
+		init = function()
+			if vim.fn.argc(-1) == 1 then
+				local stat = vim.uv.fs_stat(vim.fn.argv(0))
+				if stat and stat.type == "directory" then
+					require("neo-tree")
+				end
+			end
+		end,
+		opts = {
+			sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+			open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+			filesystem = {
+				bind_to_cwd = false,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+			},
+			window = {
+				mappings = {
+					["<space>"] = "none",
+					["Y"] = {
+						function(state)
+							local node = state.tree:get_node()
+							local path = node:get_id()
+							vim.fn.setreg("+", path, "c")
+						end,
+						desc = "Copy Path to Clipboard",
+					},
+					["O"] = {
+						function(state)
+							require("lazy.util").open(state.tree:get_node().path, { system = true })
+						end,
+						desc = "Open with System Application",
+					},
+				},
+			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
+			},
+		},
 	},
 }
