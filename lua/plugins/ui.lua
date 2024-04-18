a25fc203d5355c2291b18b2eb2eb5924ff963a3c
ret2src <2010034+ret2src@users.noreply.github.com>
2024-04-18 15:40:21 +0200

    push

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -90,7 +90,11 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("lualine").setup()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+				},
+			})
 		end,
 	},
 }
