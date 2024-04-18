1ed02bc14db0eab91c0b33b067164a95530168f8
ret2src <2010034+ret2src@users.noreply.github.com>
2024-04-18 15:47:35 +0200

    push

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -126,4 +126,35 @@ return {
 		},
 		main = "ibl",
 	},
+
+	-- animated highlight of the current indentation level
+	{
+		"echasnovski/mini.indentscope",
+		event = "VeryLazy",
+		opts = {
+			-- symbol = "▏",
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+				-- callback = function()
+				--   vim.b.miniindentscope_disable = true
+				-- end,
+			})
+		end,
+	},
 }
