18b9cdd29ba13ef15ae8827fe15121f494a56f63
ret2src <2010034+ret2src@users.noreply.github.com>
2024-04-18 15:43:19 +0200

    push

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -97,4 +97,33 @@ return {
 			})
 		end,
 	},
+
+	-- indent guides
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = "LazyFile",
+		opts = {
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+			},
+		},
+		main = "ibl",
+	},
 }
